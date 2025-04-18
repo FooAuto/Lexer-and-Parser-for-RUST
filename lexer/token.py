a612ecc5c6d5fd52a9ef3f90d781d709636bdf67
tdyfufj <3525408617@qq.com>
2025-04-18 10:06:11 +0800

    修改class tokenType(Enum)中元素的值

--- lexer/token.py
+++ lexer/token.py
@@ -1,62 +1,59 @@
 from enum import Enum
 
+
 class tokenType(Enum):
-    IDENTIFIER = 1
-    INTEGER_CONSTANT = 2
-    FLOATING_POINT_CONSTANT = 3
-    S_COMMENT = 4
-    LM_COMMENT = 5
-    RM_COMMENT = 6
-    EOF = 7
-    UNKNOWN = 8
+    IDENTIFIER = 1  # 标识符
+    MACRO_IDENTIFIER = 2  # 宏标识符（以!结尾的标识符）
+    INTEGER_CONSTANT = 3  # 整型常量
+    FLOATING_POINT_CONSTANT = 4  # 浮点型常量
+    CHAR_CONSTANT = 5  # 字符常量
+    STRING_CONSTANT = 6  # 字符串常量
+    S_COMMENT = 7  # 单行注释
+    LM_COMMENT = 8  # 多行注释开始
+    RM_COMMENT = 9  # 多行注释结束
+    EOF = 10  # 文件结束标记
+    UNKNOWN = 11  # 未知标记
 
     # 关键字
-    LET = 100
-    FN = 101
-    RETURN = 102
-    MUT = 103
-    IF = 104
-    ELSE = 105
-    WHILE = 106
-    FOR = 107
-    IN = 108
-    LOOP = 109
-    BREAK = 110
-    CONTINUE = 111
-    I32 = 112
+    I32 = 100  # i32
+    LET = 101  # let
+    IF = 102  # if
+    ELSE = 103  # else
+    WHILE = 104  # while
+    RETURN = 105  # return
+    MUT = 106  # mut
+    FN = 107  # fn
+    FOR = 108  # for
+    IN = 109  # in
+    LOOP = 110  # loop
+    BREAK = 111  # break
+    CONTINUE = 112  # continue
 
     # 各类符号
-    ASSIGN = 201      # =
-    PLUS = 202        # +
-    MINUS = 203       # -
-    MULT = 204        # *
-    DIV = 205         # /
-    EQ = 206          # ==
-    NEQ = 207         # !=
-    GT = 208          # >
-    LT = 209          # <
-    GE = 210          # >=
-    LE = 211          # <=
-    LPAREN = 212      # (
-    RPAREN = 213      # )
-    LBRACE = 214      # {
-    RBRACE = 215      # }
-    LBRACK = 216      # [
-    RBRACK = 217      # ]
-    COLON = 218       # :
-    SEMICOLON = 219   # ;
-    COMMA = 220       # ,
-    ARROW = 221       # ->
-    DOT = 222         # .
-    DOTDOT = 223      # ..
+    ASSIGN = 201  # =
+    PLUS = 202  # +
+    MINUS = 203  # -
+    MULT = 204  # *
+    DIV = 205  # /
+    EQ = 206  # ==
+    GT = 207  # >
+    GE = 208  # >=
+    LT = 209  # <
+    LE = 210  # <=
+    NEQ = 211  # !=
+    LPAREN = 212  # (
+    RPAREN = 213  # )
+    LBRACE = 214  # {
+    RBRACE = 215  # }
+    LBRACK = 216  # [
+    RBRACK = 217  # ]
+    SEMICOLON = 218  # ;
+    COLON = 219  # :
+    COMMA = 220  # ,
+    ARROW = 221  # ->
+    DOT = 222  # .
+    DOTDOT = 223  # ..
 
-    # 宏调用
-    EXCLAMATION = 99
-    MACRO_IDENTIFIER = 100
-
-    # 字符与字符串
-    CHAR_CONSTANT = 101
-    STRING_LITERAL = 102
 
 tokenKeywords = {
     "let": tokenType.LET,
@@ -98,5 +95,5 @@ tokenSymbols = {
     "->": tokenType.ARROW,
     "..": tokenType.DOTDOT,
     ".": tokenType.DOT,
-    '!': tokenType.EXCLAMATION,
+    "!": tokenType.EXCLAMATION,
 }
